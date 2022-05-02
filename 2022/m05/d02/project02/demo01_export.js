9a57d02cddf0b1ae7a8b9911de8edbd43b8db9a4
zyjava <zyjava@qq.com>
2022-05-02 21:41:54 +0800

    2022-05-02 upload..

---
+++ 2022/m05/d02/project02/demo01_export.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+function f() {
+
+}
+
+export default {
+    a,
+    f
+}
---
+++ 2022/m05/d02/project02/demo03_export.js
@@ -0,0 +1,9 @@
+export let a = 20;
+export let b = 30;
+export function f() {
+    console.log(40);
+}
+
+export default {
+    a:20
+}
